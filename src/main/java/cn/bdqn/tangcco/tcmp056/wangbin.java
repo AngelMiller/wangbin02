ad1cf3f3db457d6f30c2bcea9f2335d286e571e7
601754958 <601754958@qq.com>
2017-08-04 21:28:10 +0800

    222

--- src/main/java/cn/bdqn/tangcco/tcmp056/wangbin.java
+++ src/main/java/cn/bdqn/tangcco/tcmp056/wangbin.java
@@ -1,8 +1,15 @@
 package cn.bdqn.tangcco.tcmp056;
 
+import org.junit.Test;
+
 /**
  * Created by Dell on 2017/8/4.
  */
 public class wangbin {
-    
+
+    @Test
+    public void Test() {
+
+        System.out.println("Hello World");
+    }
 }
